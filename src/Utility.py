856a036b9f662db93fa9f9c2c50a867574b69a0e
Denis3006 <den.3006@gmail.com>
2020-07-10 10:23:26 +0200

    Revert "Revert "assignment expressions""
    This reverts commit 69602822397fe19b9e242d6ce57b887fcd1db322.

--- src/Utility.py
+++ src/Utility.py
@@ -17,8 +17,7 @@ def gender(user, male, female):
 #Выдаёт форматированный смайл с названием emote_name в формате строки для сообщения в дискорде
 #Если смайла с данным именем нет на сервере, возвращаяется пустая строка
 def emote(emote_name : str):
-    emote = discord.utils.get(Constants.GUILD.emojis, name=emote_name)
-    if emote is not None:
+    if emote := discord.utils.get(Constants.GUILD.emojis, name=emote_name):
         return str(emote)
     elif emojis.count(emojis.encode(f':{emote_name}:')) > 0:
         return emojis.encode(f':{emote_name}:')
